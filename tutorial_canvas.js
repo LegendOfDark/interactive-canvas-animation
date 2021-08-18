d6b021a3afa29a96518880470e48e9dd3214a668
LegendOfDark <67946587+LegendOfDark@users.noreply.github.com>
2021-08-18 19:25:51 -0400

    basic canvas shape tutorial

---
+++ tutorial_canvas.js
@@ -0,0 +1,53 @@
+// var canvas = document.querySelector('canvas');
+
+// canvas.width = window.innerWidth;
+// canvas.height = window.innerHeight;
+
+
+// window.addEventListener('resize', () => {
+//   canvas.width = window.innerWidth;
+//   canvas.height = window.innerHeight;
+// });
+
+// var c = canvas.getContext('2d');
+
+
+// // make a square/rectangle
+
+// // c.fillRect (100, 100 ,100 ,100 );
+// // c.fillStyle = 'red';
+
+// //Line
+
+// // c.beginPath();
+// // c.moveTo(50, 300);
+// // c.lineTo(500, 300);
+// // 
+// // c.lineTo(250, 400);
+// // c.lineTo(50,300);
+// // c.strokeStyle = 'blue';
+// // c.stroke();
+
+// //Arc / Circles
+
+// // c.beginPath();
+// // c.arc(500, 500, 30, 0, Math.PI * 2, false);
+// // c.stroke();
+
+// // Create randomized circle with unique colors
+
+// // color_num = 0;
+// // for (let r = 0; r < 100; r++)
+// // {
+// // color_num++;
+// // color = ['blue', 'green', 'red', 'yellow', 'pink']
+// // if (color_num > color.length){
+// // color_num = 0;
+// // }
+// // var x = Math.random() * canvas.width;
+// // var y = Math.random() * canvas.height;
+// // c.beginPath();
+// // c.arc(x, y, 30, 0, Math.PI * 2, false);
+// // c.strokeStyle = color[color_num];
+// // c.stroke(); 
+// // }
